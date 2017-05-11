fb1548047a22c23849c187e2747b62c9f3459c0e
alkappe <alka.cappellazzo@gmail.com>
2017-05-11 17:08:53 +0200

    first

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var cell1 = document.getElementById('cell1')
+
+cell1.onclick = function (event) {
+  var element = event.target
+  var className = element.className
+
+  if (className === 'selectedB') {
+    element.className = ''
+  } else {
+    element.className = 'selectedB'
+  }
+
+}
+
+
+var cell4 = document.getElementById('cell4')
+
+cell4.onclick = function (event) {
+  var element = event.target
+  var className = element.className
+
+  if (className === 'selectedC') {
+    element.className = ''
+  } else {
+    element.className = 'selectedC'
+  }
+}
+
+var cell7 = document.getElementById('cell7')
+
+cell7.onclick = function (event) {
+  var element = event.target
+  var className = element.className
+
+  if (className === 'selected') {
+    element.className = ''
+  } else {
+    element.className = 'selected'
+  }
+}
